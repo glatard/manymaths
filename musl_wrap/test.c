1a8065f38478d29c83e10acf5b84d966b4435234
Tristan Glatard <tristan.glatard@concordia.ca>
2021-04-13 20:12:29 -0400

    Musl wrapper

---
+++ musl_wrap/test.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+        double x = 0.2;
+        double a = cosf(x);
+        a = cos(x);
+        printf("%0.55f\n",a);
+        a = sinf(x);
+        printf("%0.55f\n",a);
+        a = sin(x);
+        printf("%0.55f\n",a);
+        printf("done\n");
+        return 0;
+}
+
