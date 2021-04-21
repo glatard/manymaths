db51c00fc0620195ed2201492f7227dc71c27493
Tristan Glatard <tristan.glatard@concordia.ca>
2021-04-20 20:47:44 -0400

    experiment script

---
+++ cmd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DATA_DIR=$HOME/data/manymaths
+LIBS="glibc-v2.31 musl-v1.1.24-1 openlibm-v0.7.0 openlibm-v0.7.5 intel-v2021.2.0"
+
+for LIB in ${LIBS}
+do
+    # Cleanup data dir
+    \rm -Rf ${DATA_DIR}/${LIB}
+    cp -Rf ${DATA_DIR}/orig ${DATA_DIR}/${LIB}
+
+    # Write command to be executed in container
+    \rm -f run.sh
+(cat <<SCRIPT
+    HOME=/usr/local/src PreFreeSurferPipelineBatch.sh --StudyFolder=${DATA_DIR}/${LIB} --Subject=101309 --runlocal
+SCRIPT
+) > run-${LIB}.sh
+
+    docker run --rm -e LIB_NAME="${LIB}" -v ${DATA_DIR}:${DATA_DIR} -w ${DATA_DIR}\
+                       pfs-manymaths ./run.sh &>run-${LIB}.log
+done
