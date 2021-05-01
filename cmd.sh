a4fad2bf7facf331bd17554e6188289d8f765d67
Tristan Glatard <tristan.glatard@concordia.ca>
2021-05-01 04:48:59 -0400

    handle large logs

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 DATA_DIR=$HOME/data/manymaths
-LIBS="glibc-v2.31 musl-v1.1.24-1 openlibm-v0.7.0 openlibm-v0.7.5 intel-v2021.2.0"
+#LIBS="glibc-v2.31 mpfr musl-v1.1.24-1 openlibm-v0.7.0 openlibm-v0.7.5 intel-v2021.2.0"
+LIBS="mpfr"
 
 for LIB in ${LIBS}
 do
@@ -11,12 +12,14 @@ do
     # Write command to be executed in container
     \rm -f ${DATA_DIR}/run-${LIB}.sh
 
+mkdir -p ${DATA_DIR}/run-${LIB}_log
 (cat <<SCRIPT
     #!/usr/bin/env bash
     HOME=/usr/local/src PreFreeSurferPipelineBatch.sh --StudyFolder=${DATA_DIR}/${LIB} --Subject=101309 --runlocal
 SCRIPT
 ) > ${DATA_DIR}/run-${LIB}.sh
     chmod 755 ${DATA_DIR}/run-${LIB}.sh
-    docker run --rm -e LIB_NAME="${LIB}" -v ${DATA_DIR}:${DATA_DIR} -w ${DATA_DIR}\
-                       pfs-manymaths ${DATA_DIR}/run-${LIB}.sh &>${DATA_DIR}/run-${LIB}.log
+    # Split the log files in chunks of 100M lines and compress them
+    docker run --rm -e LIB_NAME="${LIB}" -v ${DATA_DIR}:${DATA_DIR} -w ${DATA_DIR} --log-driver local --log-opt max-size=10m --log-opt max-file=3\
+                       pfs-manymaths ${DATA_DIR}/run-${LIB}.sh 2>&1 |  split -l 100000000 --filter 'bzip2 > "$FILE.bz2"' - ${DATA_DIR}/run-${LIB}_log/log_
 done
